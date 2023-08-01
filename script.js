c185c93fbe812e0c88e830007547a2eb8476074f
Novik Oleksandr <novikoleksandr9@gmail.com>
2023-08-01 11:56:26 +0300

    Added random spawn for food, created functions for drawing elements in canvas.

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ function Segment(x, y){
     this.y = y;
 }
 // Зюереження усіх сегментів змійки
-let snake = [
-    new Segment(10, 10),
-    new Segment(10, 11)
-]
+let snake = {
+    directionX: 1,
+    directionY: 0,
+    segments: [
+        new Segment(10, 10),
+        new Segment(10, 17)
+    ]
+}
+
 // Контейнер з грою
-const game = document.getElementById('game-container');
+const game = document.querySelector('#game-container');
+const context = game.getContext('2d');
 
 // Об'єкт конфігурації гри
 const config = {
@@ -17,40 +23,51 @@ const config = {
     height: 500,
     width: 500,
     // Зміна розмірів поля для гри
-    setResolution: function (height, width) {
-        if (isNaN(height) || isNaN(width)) {
-            console.error("NaN argument error")
+    setResolution: function (size) {
+        if (isNaN(size)) {
+            console.error("NaN argument error");
             return;
         }
-        if (height < 200 || width < 200 || height > 680) {
-            console.error("Resolution is not allowed")
+        if (size < 200 || size > 690) {
+            console.error("Resolution is not allowed [min = 200, max = 690]");
             return;
         }
-        this.height = height;
-        this.width = width;
-        game.style.width = width + "px";
-        game.style.height = height + "px";
+        this.height = size;
+        this.width = size;
+        game.style.width = this.width + "px";
+        game.style.height = this.height + "px";
     },
     // Ігрові характеристики та рахунок
     gameSpeed: 200,
     playerScore: 0,
-    cellSize: 10,
+    cellSize: 7,
     
+    // Стилізація
+    mainColor: 'rgb(0, 12, 33)',
+    secondaryColor: 'rgb(256, 256, 256)',
+
 }
-
+// Функція створення їжі 
+function spawnFood () {
+    // Додаткова функція для задання позиції їжі відповідно до розміру поля
+    function createRandomFood(minc, maxc){
+        return Math.round((Math.random() * (maxc - minc)) / config.cellSize);
+    }
+    return new Segment(
+        createRandomFood(0, config.width - config.cellSize),
+        createRandomFood(0, config.width - config.cellSize)
+    );
+}
+// Малювання їжі
+function drawFood (food){
+    context.fillStyle = config.secondaryColor;
+    context.fillRect(food.x, food.y, config.cellSize, config.cellSize);
+}
+// Малювання змійки по кожному сегменту
 function drawSnake () {
-    game.innerHTML = "";
+    context.strokeStyle = config.secondaryColor;
     //Цикл малювання кожного сегмента змійки
-    snake.forEach(segment => {
-        const segmentDiv = document.createElement('div');
-        segmentDiv.className = 'snake-segment';
-        // Задаю ширину та висоту для кожоного сегмента як вказану у конфігурації
-        segmentDiv.style.width = config.cellSize + 'px';
-        segmentDiv.style.height = config.cellSize + 'px';
-        // Задаю позицію для даного сегмента
-        segmentDiv.style.left = (segment.x * config.cellSize) + 'px';
-        segmentDiv.style.top = (segment.y * config.cellSize) + 'px';
-
-        game.appendChild(segmentDiv);
+    snake.segments.forEach(segment => {
+        context.strokeRect(segment.x, segment.y, config.cellSize, config.cellSize);
     });
 }
